e1ef7dca89e7633904500ff6d043ff963259a059
aortben <aolvaroortegabenitez03@gmail.com>
2025-11-11 21:18:38 +0100

    Controladores de categorias y login

--- src/Controller/CategoriasController.php
+++ src/Controller/CategoriasController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class CategoriasController extends AbstractController
 {
     #[Route('/categorias', name: 'app_categorias')]
-    public function index(): Response
+    public function index(CategoriaRepository $categoriaRepository): Response
     {
+        // Obtenemos todas las categorías de la base de datos
+        $categorias = $categoriaRepository->findAll();
+
         return $this->render('categorias/index.html.twig', [
-            'controller_name' => 'CategoriasController',
+            'categorias' => $categorias,
         ]);
     }
 }
+
